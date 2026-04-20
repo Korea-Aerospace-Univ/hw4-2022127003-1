06fd43150df8f886b3ac168efb43f5a1d94d8959
2022127003 <koj010919@gmail.com>
2026-04-20 16:37:11 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -5,34 +5,35 @@ int main(void)
     char ch;
     int i, count, number, len, max_num, max_str;
 
-    count = 0;
+    count=0;
     number = 0;
     max_num = 0;
     max_str = 0;
 
     scanf("%d", &len);
+    getchar();
 
-    for (i = 0; i < len; i++) {
-        scanf(" %c", &ch);
+    for (i = 0;i < len; i++) {
+        scanf("%c", &ch);
 
-        if (ch >= 'a' && ch <= 'z') 
+        if (ch >= 'a' && ch <= 'z'){ 
             count++;
             number = 0;
 
             if (count > max_str)
                 max_str = count;
         
-        else if (ch >= '0' && ch <= '9') 
+        }else if (ch >= '0' && ch <= '9'){ 
             number++;
             count = 0;
 
             if (number > max_num)
                 max_num = number;
         
-        else 
+        }else{ 
             count = 0;
             number = 0;
-        
+        }
     }
 
     printf("%d\n", max_str);
